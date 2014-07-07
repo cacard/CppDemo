0a5f6c7b5fc8dcccd83e7373cabdafa1d0f84f25
CACARD <cacard@126.com>
2014-07-07 18:00:20 +0800

    RVO / CopyAndSwap

---
+++ CPPDemo.Lang/33.CopyAndSwap.cpp
@@ -0,0 +1,25 @@
+/*
+ *  Copy and Swap 习语
+ */
+
+#include <iostream>
+using namespace std;
+
+class Class33
+{
+public:
+    Class33()
+    {
+
+    }
+
+    Class33& operator = (const Class33& that) // copy-assignment operator，使用引用做参数
+    {
+
+    }
+
+    Class33& operator = (Class33 that)// copy-assignment operator，使用值传递参数
+    {
+
+    }
+};
